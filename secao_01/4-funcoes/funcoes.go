88c2662bb70c7aaa341ddf190343122133497d38
Brunocs1991 <brunocosta.1991@hotmail.com>
2023-11-29 22:15:57 -0300

    funcoes

---
+++ secao_01/4-funcoes/funcoes.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(10, 30)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+	resultado := f("Texto da função 1")
+	fmt.Println(resultado)
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
+
+	resultadoSoma1, _ := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma1)
+
+	_, resultadoSubtracao1 := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSubtracao1)
+}
